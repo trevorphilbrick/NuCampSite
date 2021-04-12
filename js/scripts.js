ad96b7e97417665fe2c05ec7b9bf8190d45fe2b3
Trevor Philbrick <trevorphilbrick@outlook.com>
2021-04-12 12:34:05 -0400

    update

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+
+    // $("#carouselPause").click(function () {
+    //     $(".carousel").carousel("pause");
+    // });
+
+    // $("#carouselPlay").click(function () {
+    //     $(".carousel").carousel("cycle");
+});
+$("#carouselButton").click(function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+        $(".carousel").carousel("pause");
+        $("#carouselButton").children("i").removeClass("fa-pause");
+        $("#carouselButton").children("i").addClass("fa-play");
+
+    }
+    else {
+        $(".carousel").carousel("cycle");
+        $("#carouselButton").children("i").removeClass("fa-play");
+        $("#carouselButton").children("i").addClass("fa-pause");
+    }
+});
